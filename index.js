242621c8fc56145dd9be91a70167edc98a9367ba
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-06-06 08:53:05 -0400

    checking for palidromes

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for(i=0; i<word.length/2; i++) {
+    const j = word.length-1-i
+    if (word [i] !== word [j]) {
+        return false
+    } else {
+      return true
+    }
+  }
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
 
-// You can run `node index.js` to view these console logs
+
+
+
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
